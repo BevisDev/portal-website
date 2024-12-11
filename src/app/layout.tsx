38ad61567f047b2f40a1b70b8b73e2d26b7e40f3
BinhTT <truongthanh.binh@vietcredit.com.vn>
2024-12-11 15:14:28 +0700

    wrap context datetime
    (cherry picked from commit fefcf0075fbd03eb9bc7412c8d0537e39b6b6894)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,8 @@ import { Metadata } from "next";
 import { ToastContainer } from "react-toastify";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "@/components/theme/theme";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export const rootMedata: Metadata = {
   title: "Portal Website by BevisDev - Next.js 14",
@@ -26,11 +28,13 @@ const RootLayout: React.FC<RootlayoutProps> = ({ children }) => {
       </head>
       <body suppressHydrationWarning={true}>
         <AppRouterCacheProvider options={{ key: "css" }}>
-          <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <ToastContainer autoClose={3000} theme="colored" draggable />
-            {children}
-          </ThemeProvider>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <ThemeProvider theme={theme}>
+              <CssBaseline />
+              <ToastContainer autoClose={3000} theme="colored" draggable />
+              {children}
+            </ThemeProvider>
+          </LocalizationProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
